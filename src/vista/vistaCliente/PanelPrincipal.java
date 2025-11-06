96a30738c0489ee60be720ecda927924e5f16838
Norbali <norqui2007@gmail.com>
2025-11-06 08:35:15 -0500

    probando

--- src/vista/vistaCliente/PanelPrincipal.java
+++ src/vista/vistaCliente/PanelPrincipal.java
@@ -247,17 +247,16 @@ public class PanelPrincipal extends JFrame {
         boton.setCursor(new Cursor(Cursor.HAND_CURSOR));
     }
 
-    
-      public static void main(String[] args) throws IOException {
-      PanelPrincipal menu = new PanelPrincipal();
-      menu.setVisible(true);
-      menu.setSize(1300, 700);
-      ControladorCatalogo c = new ControladorCatalogo(menu);
-      ControladorActividad ca = new ControladorActividad(menu);
-      ControladorHistorial ch = new ControladorHistorial(menu);
-      ControladorSeguimiento cs = new ControladorSeguimiento(menu);
-      ControladorPQRS cpqrs = new ControladorPQRS(menu);
-      CrontoladorManejarMenu ccerrar = new CrontoladorManejarMenu(menu);
-      }
-     
+    public static void main(String[] args) throws IOException {
+        PanelPrincipal menu = new PanelPrincipal();
+        menu.setVisible(true);
+        menu.setSize(1300, 700);
+        ControladorCatalogo c = new ControladorCatalogo(menu);
+        ControladorActividad ca = new ControladorActividad(menu);
+        ControladorHistorial ch = new ControladorHistorial(menu);
+        ControladorSeguimiento cs = new ControladorSeguimiento(menu);
+        ControladorPQRS cpqrs = new ControladorPQRS(menu);
+        CrontoladorManejarMenu ccerrar = new CrontoladorManejarMenu(menu);
+    }
+
 }
