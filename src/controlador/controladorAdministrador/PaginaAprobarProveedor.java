f3598c69812e765329b4c71e8152e84302388232
CarlosQO <carlosjqo2005@gmail.com>
2025-11-08 00:07:58 -0500

    configurando el controlador

--- src/controlador/controladorAdministrador/PaginaAprobarProveedor.java
+++ src/controlador/controladorAdministrador/PaginaAprobarProveedor.java
@@ -1,5 +1,28 @@
 package controladorAdministrador;
 
-public class PaginaAprobarProveedor {
-    
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
+
+import modelo.crudProveedor.Proveedor;
+
+public class PaginaAprobarProveedor implements ActionListener {
+    private vista.vistaAdministrador.PaginaAprobarProveedor paginaAprobarProveedor;
+
+    public PaginaAprobarProveedor(vista.vistaAdministrador.PaginaAprobarProveedor paginaAprobarProveedor) {
+        this.paginaAprobarProveedor = paginaAprobarProveedor;
+        paginaAprobarProveedor.setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+    private List<Proveedor> proveedores = new ArrayList<>();
+
+    private void cargarProveedoresInactivos() {
+
+    }
 }
