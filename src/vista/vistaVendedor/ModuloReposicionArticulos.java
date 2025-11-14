6eeafd484283f2f62b619a5881a01b27d03108aa
Norbali <norqui2007@gmail.com>
2025-11-13 22:42:58 -0500

    actualizar llamadado de controladorVendedor

--- src/vista/vistaVendedor/ModuloReposicionArticulos.java
+++ src/vista/vistaVendedor/ModuloReposicionArticulos.java
@@ -6,7 +6,7 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-import controlador.controladorVendedor.controladorSolicitudesDeReposicion;
+import controladorVendedor.controladorSolicitudesDeReposicion;
 import vista.componentes.*;
 
 public class ModuloReposicionArticulos extends JPanel {
